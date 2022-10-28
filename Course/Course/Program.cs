9bfeb66fec9936e8c29699a8a0bed632183d9a55
João Pedro Fratezi <jpedro.fratezi@scopesystems.com.br>
2022-10-28 10:35:20 -0300

    PrimeiroCommit

---
+++ Course/Course/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Course 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 12;
+            Console.WriteLine(x);
+        }
+    }
+}
